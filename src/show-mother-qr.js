d768cc7808f1e57a458a26b7cb33041d34fc108c
Chris Troutner <chris.troutner@gmail.com>
2019-04-13 10:17:14 -0700

    Refactored fund-mothership into show-mother-qr

--- src/show-mother-qr.js
+++ src/show-mother-qr.js
@@ -1,11 +1,21 @@
+/*
+  This script shows the mothership wallet QR code used to fund the children.
+  This is handy in case the user needs to send additional funds to the
+  mothership address.
+*/
+
 "use strict"
+
 const qrcode = require("qrcode-terminal")
 const chalk = require("chalk")
 const emoji = require("node-emoji")
 
+const wfn = `motherShipWallet.json`
+const filename = `${__dirname}/../output/wallets/${wfn}`
+
 let mnemonicObj
 try {
-  mnemonicObj = require(`./goldenTicketWallet.json`)
+  mnemonicObj = require(filename)
 } catch (err) {
   console.log(
     `Could not open mnemonic.json. Generate a mnemonic with generate-wallet first.
@@ -16,5 +26,5 @@ try {
 
 // show funder address qr code
 console.log(`Send funds to: ${mnemonicObj.mothership.address}`)
-qrcode.generate(mnemonicObj.mothership.address)
+qrcode.generate(mnemonicObj.mothership.address, { small: true })
 console.log(chalk.green("All done."), emoji.get(":white_check_mark:"))
